95b99b4edd5131695bde223d4203737360f80abf
prayagko <koprayag@gmail.com>
2020-02-02 16:45:10 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import * as templates from './Templates.js'
+import Radar from './Radar'
 
 function App() {
+  
+  const[stats, setStats] = useState(templates.attackingMidfielder)
+  const[color, setColor] = useState('Red')
+
+  const selectTemplate = (event)=>{
+    const templateStats = (templates[event.target.value]).map(v=>{return{...v, 'value': ''}})
+    setStats(templateStats) 
+  }
+
+  const selectColor = (event) => {
+    setColor(event.target.value)
+  }
+
+  const handleInputChange = (event)=>{
+    const tempStats = [...stats]
+    tempStats[event.target.getAttribute('index')][event.target.name] = event.target.value
+    setStats(tempStats)
+  }
+
+  const deleteRow = (event) =>{
+    const tempStats = [...stats]
+    tempStats.splice(event.target.getAttribute('index'), 1)
+    setStats(tempStats)
+  }
+
+  const addRow = () => {
+    const tempStats = [...stats]
+    tempStats.push({
+      field: "",
+      min:"",
+      max:"" 
+    })
+    setStats(tempStats)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="app-header">
+        Radar Generator
+      </h1>
+      <div className="container">
+        <div className="choose">
+          <div className="options">
+            <label className="template-label">
+              Template
+            </label>
+            <select className="template-select" defaultValue = 'striker' onChange={selectTemplate}>
+              <option value="custom">Custom</option>
+              <option value="midfielder">Midfielder</option>
+              <option value="attackingMidfielder">AM/Winger</option>
+              <option value="striker">Striker</option>
+              <option value="centerback">Centerback</option>
+              <option value="fullback">Fullback</option>
+              <option value="goalkeeper">Goalkeeper</option>
+
+
+            </select>
+
+            <label className="color-label">
+              Color
+            </label>
+            <select className="color-select" onChange={selectColor}>
+              <option value="Red">Red</option>
+              <option value="Yellow">Yellow</option>
+              <option value="Gold">Gold</option>
+              <option value="Mediumpurple">Purple</option>
+              <option value="Mediumseagreen">Green</option>
+              <option value="Royalblue">Blue</option>
+            </select>
+
+          </div>
+          <div className="fields">
+            <div className="field-labels">
+              <label className="label stat">Stat</label>
+              <label className="label val">Min</label>
+              <label className="label val">Max</label>
+              <label className="label val">Value</label>
+            </div>
+            {stats.map((s,i)=>(
+              <div key={i} className="input-row">
+                
+                <input index={i} name='field' placeholder="..." className='input stat' value={s.field} onChange={handleInputChange}></input>
+                <input index={i} name='min' placeholder="" className='input val' value={s.min} onChange={handleInputChange}></input>
+                <input index={i} name='max' placeholder="" className='input val' value={s.max} onChange={handleInputChange}></input>
+                <input index={i} name='value' placeholder="" className='input val' value={s.value} onChange={handleInputChange}></input>
+                <span index={i} className="delete" onClick={deleteRow}>x</span>
+              </div>
+            ))}
+          </div>
+          <div className="buttons">
+              <button className="add" onClick={addRow}>
+                Add
+              </button>
+          </div>
+        </div>
+        <div className="radar">
+          <Radar data={stats} color={color}/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Radar.js
@@ -0,0 +1,101 @@
+import React, {useEffect, useState } from 'react'
+import * as d3 from 'd3';
+
+function Radar(props){
+
+    const width = 600;
+    const height = 600;
+
+    const[data, setData] = useState(props.data)    
+
+    useEffect(() => {  
+        setData(props.data)
+    },[props.data])
+
+  
+    function angleToCoordinate(angle, value){
+      let x = Math.cos(angle) * value;
+      let y = Math.sin(angle) * value;
+      return {"x": 300 + x, "y": 300 - y};
+  }
+    
+  function reverseScale (min, max) { 
+    return d3.scaleLinear().domain([50, 250]).range([min,max])
+  }
+
+  const chartAnnotations = [0, 50, 100, 150, 200, 250]
+    let scales = data.map((d,i)=>{
+      let angle = (Math.PI / 2) - (2 * Math.PI * i / data.length);
+
+      const r = {field: d.field,
+        func: d3.scaleLinear().domain([d.min, d.max]).range([50,250]),
+        value: d.value?(d3.scaleLinear().domain([d.min, d.max]).range([50,250]))(d.value):0,
+      }
+      if(r.value<0){
+        r.value = 0
+      }
+      if(r.value>250){
+        r.value=250
+      }
+      
+      let labelCoordinate = angleToCoordinate(angle, 270);
+      r.labelCoordinate = labelCoordinate
+      const axisValues = chartAnnotations.map((c)=>{
+        let axisCoordinates = angleToCoordinate(angle, c)
+        const v = {
+          axisValue: reverseScale(d.min, d.max)(c),
+          x: axisCoordinates.x,
+          y: axisCoordinates.y
+        }
+        return v
+        
+      })
+      r.axis = axisValues
+
+        return r
+      })
+
+
+    let line = d3.line()
+      .x(d => d.x)
+      .y(d => d.y);
+
+    function getPathCoordinates(scales){
+      let coordinates = [];
+      for (var i = 0; i < scales.length; i++){
+          let item = scales[i];
+          let angle = (Math.PI / 2) - (2 * Math.PI * i / scales.length);
+          coordinates.push(angleToCoordinate(angle, item['value'], item.func));
+      }
+      return coordinates;
+    }
+
+    const pathCoordinates = getPathCoordinates(scales)
+
+    return (
+        <svg width={width+200} height={height+200}>
+          <path fill={props.color} d={line(pathCoordinates) } opacity = {0.5}></path>
+          {scales.map((i, index) => {
+            const rotate = (360/scales.length) * index
+            return <React.Fragment key={index}>
+              (<text key={i.field} x={i.labelCoordinate.x} y={i.labelCoordinate.y}  textAnchor="middle" 
+              transform={`rotate(${rotate},${i.labelCoordinate.x},${i.labelCoordinate.y})`} 
+              fontSize= "12" fontWeight="bold">{i.field}</text>
+              {i.axis.slice(1).map((a, index)=>
+                  <text key={`${i.field}-${index}`} x={a.x} y={a.y}  textAnchor="middle"  
+                  transform={`rotate(${rotate},${a.x},${a.y})`} 
+                  fontSize= "9" fontWeight="bold">{parseFloat(a.axisValue).toFixed(2)}</text>
+          )}
+              </React.Fragment>
+          })}
+
+          {chartAnnotations.map((i) => (
+            <g key={i} transform={`translate(${width / 2}, ${height / 2})`}>
+              <circle r={i} fill="none" stroke="#999" stroke-alignment="inner" strokeWidth="25" opacity = {0.4}/>
+            </g>
+          ))}
+          </svg>  
+      )
+}
+
+export default Radar
